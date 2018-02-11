074b2dd6ffd8dc1c52c45850960dd374508e8dfc
SeanDMR <sean@flyinggrizzly.io>
2018-02-11 21:12:07 +0000

    Set up GH integrations
    - Also update readme
    - Raise an error if the slug reservation env var is misconfigured

--- config/initializers/reserved_slugs.rb
+++ config/initializers/reserved_slugs.rb
@@ -1,3 +1,10 @@
 S301::Application.configure do
-  config.reserved_slugs = %w[index unknown-short-url]
+  core_reserved_slugs = %w[index unknown-short-url]
+  instance_reserved_slugs = ENV['RESERVED_SLUGS'].split(',')
+  instance_reserved_slugs.each do |slug|
+    unless slug.match?(/^([a-z0-9]+([-_]?+[a-z0-9]+)?)*$/i)
+      raise 'RESERVED_SLUG env var must have a value like "foo,bar,baz": comma-separated, no spaces, and only letters, numbers, "-" and "_"'
+    end
+  end
+  config.reserved_slugs = core_reserved_slugs.push(*instance_reserved_slugs)
 end
