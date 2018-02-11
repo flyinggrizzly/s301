068b600bf807c3d69f7e3150613ff504fe7ce02e
SeanDMR <sean@flyinggrizzly.io>
2018-02-11 16:55:08 +0000

    Expose delete and destroy actions on short URLs

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -8,7 +8,6 @@ class ShortUrlsController < ApplicationController
   end
 
   # GET /short_urls/1
-  # GET /short_urls/1.json
   def show
   end
 
@@ -22,7 +21,6 @@ class ShortUrlsController < ApplicationController
   end
 
   # POST /short_urls
-  # POST /short_urls.json
   def create
     @short_url = ShortUrl.new(short_url_params)
     if @short_url.save
@@ -34,7 +32,6 @@ class ShortUrlsController < ApplicationController
   end
 
   # PATCH/PUT /short_urls/1
-  # PATCH/PUT /short_urls/1.json
   def update
     if @short_url.update(short_url_params)
       flash[:success] = "#{@short_url.slug} updated successfully."
@@ -44,24 +41,26 @@ class ShortUrlsController < ApplicationController
     end
   end
 
+  def delete
+    @short_url = ShortUrl.find(params[:id])
+  end
+
   # DELETE /short_urls/1
-  # DELETE /short_urls/1.json
   def destroy
     @short_url.destroy
-    respond_to do |format|
-      format.html { redirect_to short_urls_url, notice: 'Short url was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:success] = "Short URL '#{@short_url.slug}' has been deleted."
+    redirect_to short_urls_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_short_url
-      @short_url = ShortUrl.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def short_url_params
-      params.require(:short_url).permit(:slug, :redirect)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_short_url
+    @short_url = ShortUrl.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def short_url_params
+    params.require(:short_url).permit(:slug, :redirect)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :short_urls
-  
+  resources :short_urls, path: 'short-urls'
+
   root 'short_urls#index'
 end
